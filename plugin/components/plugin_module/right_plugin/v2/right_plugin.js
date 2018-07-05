77403848170107d93679d2a27fefb84c8f1aaf48
chengtao <1430125429@qq.com>
2018-07-05 14:02:15 +0800

    2018/07/05 14:02 创建权限目录

---
+++ plugin/components/plugin_module/right_plugin/v2/right_plugin.js
@@ -0,0 +1,23 @@
+// plugin/components/plugin_module/right_plugin/v1/right_plugin.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
