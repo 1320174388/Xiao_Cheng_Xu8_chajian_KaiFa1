d5f513808f6ced75cac7396f7897baff1d183542
chengtao <1430125429@qq.com>
2018-07-05 19:40:32 +0800

    2018/07/05 19:40 时间修改

--- plugin/components/plugin_module/admin_plugin/v1/admin_plugin.js
+++ plugin/components/plugin_module/admin_plugin/v1/admin_plugin.js
@@ -6,6 +6,12 @@
  *  文件描述 :  后台首页js文件
  *  历史记录 :  -----------------------
  */
+var time = new Date();
+var M = time.getMonth() + 1;
+var D = time.getDate();
+var X = time.getDay();
+var week = ["日", "一", "二", "三", "四", "五", "六"]
+var strTime = M + '月' + D + '日'+' 星期'+week[X];
 Component({
   /**
    * 组件的属性列表
@@ -24,7 +30,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+      time: strTime,
+      
   },
 
   /**
--- plugin/components/plugin_module/apply_plugin/v1/apply_plugin.js
+++ plugin/components/plugin_module/apply_plugin/v1/apply_plugin.js
@@ -6,25 +6,33 @@
  *  文件描述 :  后台首页js文件
  *  历史记录 :  -----------------------
  */
+var time = new Date();
+var M = time.getMonth()+1;
+var D = time.getDate();
+var X = time.getDay();
+var week = ["日", "一", "二", "三", "四", "五", "六"]
+var strTime = M + '月' + D + '日' + ' 星期' + week[X];
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
 
-  },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        time: strTime,
+    },
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        timeh: function() {
+            
+        }
+    }
 })
